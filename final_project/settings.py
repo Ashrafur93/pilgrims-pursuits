18cd700fc5d3da7f808c6a81064d7c5cd3be34e3
Ashrafur93 <ashrafur_ahmed@hotmail.co.uk>
2025-02-03 19:06:47 +0000

    Use environment variable for SECRET_KEY in settings

--- final_project/settings.py
+++ final_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8-k*3*jh@f_b-s^b@33c2jyr6-bv7-30!x_e2pua1rq4#bgswb'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
