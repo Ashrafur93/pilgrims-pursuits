66b130092d36a4614c2939bcae511816887a48bb
Ashrafur93 <ashrafur_ahmed@hotmail.co.uk>
2025-02-10 02:39:40 +0000

    Amend About view and URL to use lowercase naming convention; streamline collaboration form handling

--- about/urls.py
+++ about/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.About, name='about'),
+    path('', views.about, name='about'),
 ]
--- about/views.py
+++ about/views.py
@@ -10,14 +10,18 @@ def about(request):
     Renders the About page
     """
 
+    about = About.objects.all().order_by('-updated_on').first()
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
             collaborate_form.save()
             messages.add_message(request, messages.SUCCESS, "Collaboration request received! We endeavour to respond within 2 working days.")
 
-    about = About.objects.all().order_by('-updated_on').first()
-    collaborate_form = CollaborateForm()
+            collaborate_form = CollaborateForm()
+        else:
+            collaborate_form = CollaborateForm()
+    else:
+        collaborate_form = CollaborateForm()            
 
     return render(
         request,
